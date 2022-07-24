45aee90dfa6053bb49f988b1170c3ad9630872b7
Daniel M. Lofaro <dan@danlofaro.com>
2022-07-24 11:33:32 +0900

    poly seems to be working, need to make it so i can turn off a note too

--- line_audio_poly.py
+++ line_audio_poly.py
@@ -2,13 +2,17 @@ from pythonosc import udp_client
 import time
 
 client = udp_client.SimpleUDPClient("127.0.0.1", 57120) #default ip and port for SC
-client.send_message("/print", 440) # set the frequency at 440
 
-f = 440
-while True:
-  f = f + f/100.0
-  client.send_message('/key0', f) # set the frequency at 440
-  client.send_message('/key1', f/2.0) # set the frequency at 440
-  client.send_message('/key2', f*2.0) # set the frequency at 440
-  break
-  time.sleep(100.0)
+f = 450
+#while True:
+f = f + f/100.0
+ # client.send_message('/key0', f) # set the frequency at 440
+ # client.send_message('/key1', f/2.0) # set the frequency at 440
+client.send_message('/key0', 100) # set the frequency at 440
+client.send_message('/key1', 200) # set the frequency at 440
+client.send_message('/key2', 500) # set the frequency at 440
+client.send_message('/key3', 1000) # set the frequency at 440
+client.send_message('/key4', 2000) # set the frequency at 440
+client.send_message('/key5', 5000) # set the frequency at 440
+#  break
+time.sleep(0.5)
